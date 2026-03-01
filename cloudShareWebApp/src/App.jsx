1ae1864b87134362748e6c08f4cfbb0f3045a848
soumen kumar bera <soumenbera4017@gmail.com>
2026-03-01 09:32:18 +0530

    google font setup

--- cloudShareWebApp/src/App.jsx
+++ cloudShareWebApp/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   
 
-  return <div className="text-4xl text-red-400"> app content</div>
+  return <div className=""> app content</div>
 }
 
 export default App;
