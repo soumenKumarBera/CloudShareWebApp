04db3e161ac0c6da06d2e129db6dd552c0902b98
soumen kumar bera <soumenbera4017@gmail.com>
2026-03-07 23:01:02 +0530

    Footer ready

--- cloudShareWebApp/src/componnets/landing/FooterSection.jsx
+++ cloudShareWebApp/src/componnets/landing/FooterSection.jsx
@@ -1,6 +1,17 @@
 export const FooterSection = ()  =>{
   return <div className="bg-gray-800">
-    
+    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div className="text-center">
+        <p className="text-base text-gray-400">
+          &copy; 2025 CloudShare. All right reserved.
+
+        </p>
+
+      </div>
+
+    </div>
+
+
   </div>
 }
 
